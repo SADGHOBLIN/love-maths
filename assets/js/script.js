04d945d3ad7b10ce484b944e6da86e83f4143225
sadghoblin <171681941+SADGHOBLIN@users.noreply.github.com>
2025-10-17 12:10:18 +0100

    Add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,6 +75,9 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
 
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtraction"];
+
     } else if (operator === "x") {
         return [operand1 * operand2, "multiplication"];
 
@@ -109,6 +112,9 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
